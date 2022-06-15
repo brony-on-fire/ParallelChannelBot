5c65e8f791b455eb041e894951a9f45ba1702710
Vasily <vshmkv@473933-cr76734.tmweb.ru>
2022-06-16 01:12:12 +0700

    Добавлена работающая кнопка с обсуждением

--- app/src/handlers/posting.py
+++ app/src/handlers/posting.py
@@ -1,7 +1,8 @@
 from aiogram import types, Dispatcher
 from aiogram.dispatcher.filters import ChatTypeFilter
+from time import time
 from src.bases.db_operations import ChatPosting, ChannelPosting, ComplainAction
-from src.markups.markups import newComplain
+from src.markups.markups import mainMenu, comments_button
 from create_bot import dp, bot
 
 async def posting(message:types.Message):
@@ -20,11 +21,34 @@ async def posting(message:types.Message):
             await message.reply(message_for_answer)
         else:
             channel = check_permission['message']
-            save_post = await bot.send_message(channel, message_for_post, reply_markup=newComplain)
+            save_post = await bot.send_message(channel, message_for_post)
+
+            #Сохраняем пост в БД
             save_post = ChannelPosting(save_post)
             save_post.save_post(author = message.from_user.id)
 
+async def create_comment_button(message:types.Message):
+    '''
+    Создаёт кнопку со ссылкой на обсуждение в сообщении канала
+    '''
+    if message.is_automatic_forward:
+        #Получаем информацию о посте
+        channel_id = message.sender_chat.id
+        post_id = message.forward_from_message_id
+
+        #Формируем кнопку обсуждения
+        message_id_for_url = message.message_id
+        chat_id_for_url = str(message.chat.id)[4:]
+        
+        comments_button.url = f'https://t.me/c/{chat_id_for_url}/{message_id_for_url}?thread={message_id_for_url}'
+
+        #Добавляем к посту клавиатуру
+        await bot.edit_message_reply_markup(chat_id=channel_id, message_id=post_id, reply_markup=mainMenu)
+
 async def complain(call:types.CallbackQuery):
+    '''
+    Обрабатывает нажатие на кнопку жалобы
+    '''
     author = call.from_user.id
 
     #Сохраняем жалобу в БД
@@ -44,4 +68,5 @@ async def complain(call:types.CallbackQuery):
 
 def register_handlers_posting(dp:Dispatcher):
     dp.register_message_handler(posting, ChatTypeFilter(chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP]), commands=['post'])
+    dp.register_message_handler(create_comment_button, ChatTypeFilter(chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP]), content_types=['text'])
     dp.register_callback_query_handler(complain, text_contains='complain')
--- app/src/markups/markups.py
+++ app/src/markups/markups.py
@@ -1,9 +1,9 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 #Клавиатура для добавления жалобы
-newComplain = InlineKeyboardMarkup(row_width=2, one_time_keyboard=True)
+mainMenu = InlineKeyboardMarkup(row_width=2, one_time_keyboard=True)
 complain = InlineKeyboardButton(text='Жалоба ⚠️', callback_data='complain')
-comments = InlineKeyboardButton(text='Обсуждение 💬', callback_data='go_to_comments')
+comments_button = InlineKeyboardButton(text='Обсуждение 💬', url = 'https://telegram.org/')
 
-newComplain.insert(complain)
-newComplain.insert(comments)
+mainMenu.insert(complain)
+mainMenu.insert(comments_button)
