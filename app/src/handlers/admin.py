209d944a6eab656db622108e99d6f0427080ebe9
Vasily <vshmkv@473933-cr76734.tmweb.ru>
2022-06-18 20:29:15 +0700

    Добавлен ответ для help команды.

--- app/src/handlers/admin.py
+++ app/src/handlers/admin.py
@@ -9,6 +9,20 @@ async def get_id(message: types.Message):
     '''
     await message.reply(message.chat.id)
 
+async def help_bot(message: types.Message):
+    '''
+    Выводит подсказку по боту в канал
+    '''
+    await message.reply('Для полной работоспособности бота необходимо: назначить бота админом в канале, '\
+                        'добавить бота в связанный чат обсуждения канала и назначить его админом, командой '\
+                        '"ink_chat <id_чата>" связать канал с чатом (получить id чата можно в самом чате '\
+                        'по команде /get_id). Используйте в канале "mute_timer <число>", чтобы установить '\
+                        'длительность временной блокировки пользователей, на посты которых поступило много '\
+                        'жалоб (по умолчанию - 3 дня). Используйте в канале "votes_for_block <число>", '\
+                        'чтобы установить количество жалоб, необходимых для блокировки пользователя '\
+                        '(по умолчанию - 10).')
+
+
 async def link_chat(message: types.Message):
     '''
     Связывает канал с чатом
@@ -34,7 +48,7 @@ async def mute_timer(message: types.Message):
         update_timer = ChannelSetting(message).set_setting("mute_timer", mute_timer_value)
         await message.reply(f'{update_timer}')
 
-async def vote_for_block(message: types.Message):
+async def votes_for_block(message: types.Message):
     '''
     Задает количество жалоб для блока
     '''
@@ -48,6 +62,7 @@ async def vote_for_block(message: types.Message):
 
 def register_handlers_admin(dp:Dispatcher):
     dp.register_message_handler(get_id, ChatTypeFilter(chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP]), commands=['get_id'])
+    dp.register_channel_post_handler(help_bot, ChatTypeFilter(chat_type=[types.ChatType.CHANNEL]), text_startswith=['help'])
     dp.register_channel_post_handler(link_chat, ChatTypeFilter(chat_type=[types.ChatType.CHANNEL]), text_startswith=['link_chat '])
     dp.register_channel_post_handler(mute_timer, ChatTypeFilter(chat_type=[types.ChatType.CHANNEL]), text_startswith=['mute_timer '])
-    dp.register_channel_post_handler(vote_for_block, ChatTypeFilter(chat_type=[types.ChatType.CHANNEL]), text_startswith=['vote_for_block '])
+    dp.register_channel_post_handler(votes_for_block, ChatTypeFilter(chat_type=[types.ChatType.CHANNEL]), text_startswith=['votes_for_block '])
